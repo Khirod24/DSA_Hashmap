c99b71def736ff56e8984b08a924a7e900979736
D Khirod Kumar <113640893+Khirod24@users.noreply.github.com>
2023-12-31 17:30:13 +0530

    Create map_arr.cpp

---
+++ map_arr.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+    map<int,int>mp;
+    int n;
+    cout<<"ENTER THE NUMBER OF ELEMENTS: ";
+    cin>>n;
+    cout<<"ENTER THE ELEMENTS: ";
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+        mp[arr[i]]++;
+    }
+    
+    //iterate in the map
+    for(auto it:mp){
+        cout<<it.first<<"->"<<it.second<<endl;
+    }
+    
+    cout<<"ENTER NUMBER OF ELEMENTS TO FIND COUNT: ";
+    int q;
+    cin>>q;
+    cout<<"ENTER ALL ELEMENTS: ";
+    while(q--){
+        int k;
+        cin>>k;
+        cout<<"Count of "<<k<<" is: "<<mp[k]<<endl;
+    }
+    
+}
